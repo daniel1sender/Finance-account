1e656e669cb4913888e86b1c6c1bfcd2e158407c
Daniel Sender <daniel.saul7@gmail.com>
2021-08-23 14:15:05 -0300

    test: refactor test of transfer use case

--- pkg/domain/transfers/make_transfer_test.go
+++ pkg/domain/transfers/make_transfer_test.go
@@ -13,8 +13,11 @@ func TestAccountUseCase_MakeTransfer(t *testing.T) {
 
 		storage := transfers.NewTransferStorage()
 		TransferUsecase := NewTransferUseCase(storage)
+		amount := 10
+		originID := 1
+		destinationID := 2
 
-		MakeTransfer, err := TransferUsecase.MakeTransfer(1, 2, 10)
+		MakeTransfer, err := TransferUsecase.MakeTransfer(originID, destinationID, amount)
 
 		if MakeTransfer == (entities.Transfer{}) {
 			t.Errorf("Expected a transfer but got %+v", MakeTransfer)
