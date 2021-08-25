d8f995393283f5e1355181a5ab6b435a57a62e20
Daniel Sender <daniel.saul7@gmail.com>
2021-08-25 15:52:46 -0300

    style: add new sentinel error to account domain use case

--- pkg/domain/accounts/create_account.go
+++ pkg/domain/accounts/create_account.go
@@ -8,12 +8,13 @@ import (
 
 var (
 	ErrToCreateNewAccount = errors.New("error to create a new account")
+	ErrExistingCPF        = errors.New("cpf informed is invalid")
 )
 
 func (au AccountUseCase) CreateAccount(name, cpf, secret string, balance int) (entities.Account, error) {
 
 	if err := au.storage.CheckCPF(cpf); err != nil {
-		return entities.Account{}, err
+		return entities.Account{}, ErrExistingCPF
 	}
 
 	account, err := entities.NewAccount(name, cpf, secret, balance)
