307ff980b3dab14b38353a7cb6e6c0e21cfe2141
Daniel Sender <daniel.saul7@gmail.com>
2021-08-25 15:08:18 -0300

    style: change the name offunction scoped variable 'AccountsList' to 'accountlist'

--- pkg/store/accounts/get_accounts.go
+++ pkg/store/accounts/get_accounts.go
@@ -3,9 +3,9 @@ package accounts
 import "github.com/daniel1sender/Desafio-API/pkg/domain/entities"
 
 func (s AccountStorage) GetAccounts() []entities.Account {
-	var AccountsList []entities.Account
+	var accountsList []entities.Account
 	for _, value := range s.storage {
-		AccountsList = append(AccountsList, value)
+		accountsList = append(accountsList, value)
 	}
-	return AccountsList
+	return accountsList
 }
