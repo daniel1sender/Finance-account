a4d6b97c01ec69445d16c58a9a829d3939a1a4ec
Daniel Sender <daniel.saul7@gmail.com>
2021-08-26 11:24:59 -0300

    style: change the subtestdescription to a more especific one

--- pkg/domain/accounts/get_balance_by_id_test.go
+++ pkg/domain/accounts/get_balance_by_id_test.go
@@ -36,7 +36,7 @@ func TestAccountUseCase_GetBalanceByID(t *testing.T) {
 
 	})
 
-	t.Run("should return a blank account when id isn't found", func(t *testing.T) {
+	t.Run("should return a null account balance when id isn't found", func(t *testing.T) {
 
 		storage := accounts.NewAccountStorage()
 		AccountUseCase := NewAccountUseCase(storage)
