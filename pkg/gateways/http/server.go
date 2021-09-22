9447cb2e8a422ed3e38ec54c8bb881d5132fcf65
Daniel Sender <daniel.saul7@gmail.com>
2021-09-22 11:32:26 -0300

    chore: create a server path to coordinate routes

--- pkg/gateways/http/server.go
+++ pkg/gateways/http/server.go
@@ -1,17 +1,5 @@
 package http
 
-import "github.com/daniel1sender/Desafio-API/pkg/domain/accounts"
-
 const (
 	ContentType = "application/json"
 )
-
-type Handler struct {
-	useCase accounts.AccountUseCase
-}
-
-func NewHandler(useCase accounts.AccountUseCase) Handler {
-	return Handler{
-		useCase: useCase,
-	}
-}
