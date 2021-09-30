8cedbb73856d742d880d426af8d4a2340130523a
Daniel Sender <daniel.saul7@gmail.com>
2021-09-30 12:10:04 -0300

    feat: add use cases mocking to get handler test

--- pkg/domain/accounts/use_case_mock.go
+++ pkg/domain/accounts/use_case_mock.go
@@ -1,12 +1,16 @@
 package accounts
 
 import (
+	"fmt"
+
 	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
 )
 
 type UseCaseMock struct {
 	Balance int
-	Error error
+	Error   error
+	Account entities.Account
+	List    []entities.Account
 }
 
 func (m *UseCaseMock) GetBalanceByID(id string) (int, error) {
@@ -14,7 +18,15 @@ func (m *UseCaseMock) GetBalanceByID(id string) (int, error) {
 }
 
 func (m *UseCaseMock) Create(name, cpf, secret string, balance int) (entities.Account, error) {
-	panic("not implemented")
+
+	account, err := entities.NewAccount(name, cpf, secret, balance)
+	if err != nil {
+		return entities.Account{}, fmt.Errorf("%s: %w", ErrCreatingNewAccount, err)
+	}
+
+	m.List = append(m.List, account)
+
+	return account, nil
 }
 
 func (m *UseCaseMock) GetByID(id string) (entities.Account, error) {
@@ -22,7 +34,7 @@ func (m *UseCaseMock) GetByID(id string) (entities.Account, error) {
 }
 
 func (m *UseCaseMock) Get() []entities.Account {
-	panic("not implemented")
+	return m.List
 }
 
 func (m *UseCaseMock) UpdateBalance(id string, balance int) error {
