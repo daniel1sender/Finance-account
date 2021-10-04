46deb09916459a3b4b2f05511a3608c1349c87a3
Daniel Sender <daniel.saul7@gmail.com>
2021-10-04 16:49:57 -0300

    feat: add wrap in errors to give more context

--- pkg/domain/transfers/make.go
+++ pkg/domain/transfers/make.go
@@ -2,10 +2,12 @@ package transfers
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
 )
 
+//checar depois esse erro deve ser uma sentinela
 var (
 	ErrCreatingNewTransfer = errors.New("error when creating a transfer")
 )
@@ -15,7 +17,7 @@ func (tu *TransferUseCase) Make(originID, destinationID int, amount int) (entiti
 	transfer, err := entities.NewTransfer(originID, destinationID, amount)
 
 	if err != nil {
-		return entities.Transfer{}, ErrCreatingNewTransfer
+		return entities.Transfer{}, fmt.Errorf("error while creating transfer: %w", err)
 	}
 
 	tu.storage.UpdateByID(transfer.ID, transfer)
