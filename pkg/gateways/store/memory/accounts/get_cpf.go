b63fa2be0cd7be3db0d632bfc47d9d2a8bf3b124
Daniel Sender <daniel.saul7@gmail.com>
2021-11-10 13:55:51 -0300

    refactor: refactor create account method

--- pkg/gateways/store/memory/accounts/get_cpf.go
+++ pkg/gateways/store/memory/accounts/get_cpf.go
@@ -1,9 +1,11 @@
 package accounts
 
+import "github.com/daniel1sender/Desafio-API/pkg/gateways/store"
+
 func (s AccountStorage) CheckCPF(cpf string) error {
 	for _, storedAccount := range s.storage {
 		if storedAccount.CPF == cpf {
-			return ErrExistingCPF
+			return store.ErrExistingCPF
 		}
 	}
 	return nil
