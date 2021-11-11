bc697d46f5a8867e8a1493460ccdaea3f494007a
Daniel Sender <daniel.saul7@gmail.com>
2021-11-11 15:31:25 -0300

    feat: implement check cpf method

--- pkg/gateways/store/repository/accounts/new_repository.go
+++ pkg/gateways/store/repository/accounts/new_repository.go
@@ -1,6 +1,9 @@
 package accounts
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"time"
@@ -8,22 +11,40 @@ import (
 
 type AccountRepository struct {
 	storage *os.File
+	Users   map[string]AccountResponse
 }
 
 type AccountResponse struct {
-	ID        string    `json:"id"`
+	//	ID        string    `json:"id"`
 	Name      string    `json:"name"`
 	CPF       string    `json:"cpf"`
 	Balance   int       `json:"balance"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
+type Account struct {
+	Users map[string]AccountResponse
+}
+
 func NewStorage() AccountRepository {
-	file, err := os.OpenFile("repository", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	openFile, err := os.OpenFile("repository.json", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
+	accountMap := make(map[string]AccountResponse)
+
+	readFile, err := ioutil.ReadAll(openFile)
+	if err != nil {
+		return AccountRepository{}
+	}
+
+	err = json.Unmarshal(readFile, &accountMap)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	return AccountRepository{
-		storage: file,
+		storage: openFile,
+		Users:   accountMap,
 	}
 }
--- pkg/gateways/store/repository/accounts/upsert.go
+++ pkg/gateways/store/repository/accounts/upsert.go
@@ -2,16 +2,20 @@ package accounts
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
 
 	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
 )
 
 func (ar AccountRepository) Upsert(account entities.Account) {
-	accountResponse := AccountResponse{account.ID, account.Name, account.CPF, account.Balance, account.CreatedAt}
-	keepAccount, err := json.Marshal(accountResponse)
+	users := ar.Users
+	users[account.ID] = AccountResponse{account.Name, account.CPF, account.Balance, account.CreatedAt}
+
+	keepAccount, err := json.MarshalIndent(users, "", " ")
 	if err != nil {
 		log.Fatal("error decoding account")
 	}
-	ar.storage.WriteString(string(keepAccount) + "\n")
+	//ar.storage.WriteString(string(keepAccount) + "\n")
+	_ = ioutil.WriteFile("repository.json", keepAccount, 0644)
 }
