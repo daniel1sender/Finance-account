342b89e4b43e7a3dab9b797d04951ea3f5cb2070
Daniel Sender <daniel.saul7@gmail.com>
2021-11-12 13:31:03 -0300

    feat: add get balance by id method

--- pkg/gateways/store/memory/accounts/get_by_id.go
+++ pkg/gateways/store/memory/accounts/get_by_id.go
@@ -1,11 +1,14 @@
 package accounts
 
-import "github.com/daniel1sender/Desafio-API/pkg/domain/entities"
+import (
+	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
+	"github.com/daniel1sender/Desafio-API/pkg/gateways/store"
+)
 
 func (s AccountStorage) GetByID(id string) (entities.Account, error) {
 	account, ok := s.storage[id]
 	if !ok {
-		return entities.Account{}, ErrIDNotFound
+		return entities.Account{}, store.ErrIDNotFound
 	}
 	return account, nil
 }
