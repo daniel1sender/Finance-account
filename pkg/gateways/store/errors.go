feb42db66429bba05d3a9fce0e839556428d15f9
Daniel Sender <daniel.saul7@gmail.com>
2021-11-12 13:31:03 -0300

    feat: add get balance by id method

--- pkg/gateways/store/errors.go
+++ pkg/gateways/store/errors.go
@@ -4,4 +4,5 @@ import "errors"
 
 var (
 	ErrExistingCPF = errors.New("cpf informed alredy exists")
+	ErrIDNotFound  = errors.New("account id isn't found")
 )
--- pkg/gateways/store/repository/accounts/get_balance_by_id.go
+++ pkg/gateways/store/repository/accounts/get_balance_by_id.go
@@ -1,5 +1,12 @@
 package accounts
 
+import "github.com/daniel1sender/Desafio-API/pkg/gateways/store"
+
 func (ar AccountRepository) GetBalanceByID(id string) (int, error) {
-	panic("not implemented")
+	_, ok := ar.Users[id]
+	if !ok {
+		return 0, store.ErrIDNotFound
+	}
+	balance := ar.Users[id].Balance
+	return balance, nil
 }
