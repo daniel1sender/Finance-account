fa8022f7c404360694037ed6923af698e797d847
Daniel Sender <daniel.saul7@gmail.com>
2021-11-12 13:54:32 -0300

    test: add test to get all method

--- pkg/gateways/store/repository/accounts/get_all_test.go
+++ pkg/gateways/store/repository/accounts/get_all_test.go
@@ -1,2 +1,31 @@
 package accounts
 
+import (
+	"testing"
+
+	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
+)
+
+func TestGetAll(t *testing.T) {
+
+	t.Run("should return a list of accounts when accounts have already been created", func(t *testing.T) {
+		storage := NewStorage()
+		account := entities.Account{ID: "123", Balance: 10}
+		storage.Users[account.ID] = account
+		returnedAccount := storage.GetAll()
+
+		if len(returnedAccount) == 0 {
+			t.Errorf("expected a full list of accounts")
+		}
+	})
+
+	t.Run("should return a list of accounts when accounts don't have already been created", func(t *testing.T) {
+		storage := NewStorage()
+		returnedAccount := storage.GetAll()
+
+		if len(returnedAccount) != 0 {
+			t.Errorf("expected an empty list of accounts")
+		}
+	})
+
+}
