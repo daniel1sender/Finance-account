cce1738517c66e140245956bf5735bd547434387
Daniel Sender <daniel.saul7@gmail.com>
2021-11-23 18:48:28 -0300

    refactor: improve the return of account repository implementation

--- pkg/gateways/store/memory/accounts/upsert.go
+++ pkg/gateways/store/memory/accounts/upsert.go
@@ -2,6 +2,7 @@ package accounts
 
 import "github.com/daniel1sender/Desafio-API/pkg/domain/entities"
 
-func (s AccountStorage) Upsert(account entities.Account) {
+func (s AccountStorage) Upsert(account entities.Account) error {
 	s.storage[account.ID] = account
+	return nil
 }
