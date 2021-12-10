13ea42bb94c62c2cea0bcadc48a80a889d515913
Daniel Sender <daniel.saul7@gmail.com>
2021-12-10 08:41:23 -0300

    style: improve error handling

--- pkg/domain/accounts/usecases/create.go
+++ pkg/domain/accounts/usecases/create.go
@@ -3,14 +3,13 @@ package usecases
 import (
 	"fmt"
 
-	"github.com/daniel1sender/Desafio-API/pkg/domain/accounts"
 	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
 )
 
 func (au AccountUseCase) Create(name, cpf, secret string, balance int) (entities.Account, error) {
 
 	if err := au.storage.CheckCPF(cpf); err != nil {
-		return entities.Account{}, accounts.ErrExistingCPF
+		return entities.Account{}, fmt.Errorf("error while creating an account: %w", err)
 	}
 
 	account, err := entities.NewAccount(name, cpf, secret, balance)
