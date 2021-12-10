595be753e1b3e12640d134d592c90f4076b32045
Daniel Sender <daniel.saul7@gmail.com>
2021-12-10 08:39:25 -0300

    style: remove unsued code

---
+++ pkg/gateways/migrations/000002_create_transfer_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS transfers(
+    id UUID PRIMARY KEY,
+    account_origin_id UUID NOT NULL,
+    account_destination_id UUID NOT NULL,
+    amount int,
+    create_at TIMESTAMP WITH TIME ZONE NOT NULL 
+);
+
