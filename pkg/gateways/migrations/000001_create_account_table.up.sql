99c8d54580993d8dad3a0339933c40b446935154
Daniel Sender <daniel.saul7@gmail.com>
2021-12-10 10:12:43 -0300

    chore: remove migrations

--- pkg/gateways/migrations/000001_create_account_table.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS accounts(
-    id UUID PRIMARY KEY,
-    name text NOT NULL,
-    cpf text NOT NULL,
-    secret text NOT NULL,
-    balance int NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL
-);
