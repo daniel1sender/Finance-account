e12fc962bcd43df55a90884103b7a7bd3afd33a8
Daniel Sender <daniel.saul7@gmail.com>
2021-12-10 09:43:15 -0300

    style: improve error handling of methods

--- pkg/domain/accounts/usecases/get_by_id.go
+++ pkg/domain/accounts/usecases/get_by_id.go
@@ -1,10 +1,15 @@
 package usecases
 
 import (
+	"fmt"
+
 	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
 )
 
 func (au AccountUseCase) GetByID(id string) (entities.Account, error) {
 	account, err := au.storage.GetByID(id)
+	if err != nil {
+		return entities.Account{}, fmt.Errorf("error getting account by id: %w", err)
+	}
 	return account, err
 }
--- pkg/domain/transfers/make.go
+++ pkg/domain/transfers/make.go
@@ -2,6 +2,7 @@ package transfers
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
 )
@@ -13,13 +14,12 @@ var (
 func (tu TransferUseCase) Make(originID, destinationID string, amount int) (entities.Transfer, error) {
 
 	transfer, err := entities.NewTransfer(originID, destinationID, amount)
-
 	if err != nil {
-		return entities.Transfer{}, ErrCreatingNewTransfer
+		return entities.Transfer{}, fmt.Errorf("error when creating a transfer: %w", err)
 	}
 
 	err = tu.storage.UpdateByID(transfer)
-	if err != nil{
+	if err != nil {
 		return entities.Transfer{}, err
 	}
 
