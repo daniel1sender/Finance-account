2966764e7d0df665c3bad3496c8454e991504007
Daniel Sender <daniel.saul7@gmail.com>
2021-12-10 11:38:21 -0300

    style: change restrictions to improve bank security

--- pkg/gateways/migrations/000001_create_accounts_table.up.sql
+++ pkg/gateways/migrations/000001_create_accounts_table.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS accounts(
 id UUID PRIMARY KEY,
 name text NOT NULL,
-cpf text NOT NULL,
-secret text NOT NULL,
+cpf text UNIQUE NOT NULL,
+secret text UNIQUE NOT NULL,
 balance int NOT NULL,
 createdAt TIMESTAMP WITH TIME ZONE NOT NULL
 );
--- pkg/gateways/migrations/000002_create_transfers_table.up.sql
+++ pkg/gateways/migrations/000002_create_transfers_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS transfers(
 id UUID PRIMARY KEY,
-account_origin_id UUID NOT NULL,
-account_destination_id UUID NOT NULL,
-amount int,
+account_origin_id UUID UNIQUE NOT NULL,
+account_destination_id UUID UNIQUE NOT NULL,
+amount int NOT NULL,
 create_at TIMESTAMP WITH TIME ZONE NOT NULL
 );
