fcf87b369773c15cb72113a7e51e276703309f41
Daniel Sender <daniel.saul7@gmail.com>
2021-12-15 08:51:43 -0300

    style: improve types and constraints of migrations

--- pkg/gateways/migrations/000002_create_transfers_table.up.sql
+++ pkg/gateways/migrations/000002_create_transfers_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS transfers(
 id UUID PRIMARY KEY,
-account_origin_id UUID UNIQUE NOT NULL,
-account_destination_id UUID UNIQUE NOT NULL,
-amount int NOT NULL,
-create_at TIMESTAMP WITH TIME ZONE NOT NULL
+account_origin_id UUID UNIQUE,
+account_destination_id UUID UNIQUE,
+amount BIGINT NOT NULL,
+created_at TIMESTAMP WITH TIME ZONE NOT NULL
 );
