9535fd62cb983eaeabf6fd517469af7506fddd57
Daniel Sender <daniel.saul7@gmail.com>
2021-12-27 14:46:15 -0300

    style: fix constraints of schema migrations to be following my business rule

--- pkg/gateways/migrations/000002_create_transfers_table.up.sql
+++ pkg/gateways/migrations/000002_create_transfers_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS transfers(
 id UUID PRIMARY KEY,
-account_origin_id UUID UNIQUE,
-account_destination_id UUID UNIQUE,
+account_origin_id UUID,
+account_destination_id UUID,
 amount BIGINT NOT NULL,
 created_at TIMESTAMP WITH TIME ZONE NOT NULL
 );
