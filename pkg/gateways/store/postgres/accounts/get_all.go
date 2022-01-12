66a68b9c0bbbf91f8efc50489034cf66ea4db999
Daniel Sender <daniel.saul7@gmail.com>
2022-01-12 17:46:23 -0300

    feat: add method to list all accounts

---
+++ pkg/gateways/store/postgres/accounts/get_all.go
@@ -0,0 +1,27 @@
+package accounts
+
+import (
+	"context"
+	"log"
+
+	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
+)
+
+func (ar AccountRepository) GetAll() []entities.Account {
+	var users []entities.Account
+	var user entities.Account
+
+	rows, err := ar.Query(context.Background(), "SELECT ID, NAME, CPF, SECRET, BALANCE, CREATEDAT FROM ACCOUNTS")
+	if err != nil {
+		log.Printf("error to read table rows: %v", err)
+	}
+
+	for rows.Next() {
+		err := rows.Scan(&user.ID, &user.Name, &user.CPF, &user.Secret, &user.Balance, &user.CreatedAt)
+		if err != nil {
+			log.Print(err)
+		}
+		users = append(users, user)
+	}
+	return users
+}
