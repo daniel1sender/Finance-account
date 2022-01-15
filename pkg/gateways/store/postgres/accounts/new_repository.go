c6ff05024371f8ad8fc7641cd46ed43ac97d3a9d
Daniel Sender <daniel.saul7@gmail.com>
2022-01-15 12:18:58 -0300

    fix: adjust builder function to receive a specified db connection

--- pkg/gateways/store/postgres/accounts/new_repository.go
+++ pkg/gateways/store/postgres/accounts/new_repository.go
@@ -1,9 +1,6 @@
 package accounts
 
 import (
-	"context"
-	"log"
-
 	pgx "github.com/jackc/pgx/v4/pgxpool"
 )
 
@@ -11,10 +8,6 @@ type AccountRepository struct {
 	*pgx.Pool
 }
 
-func NewStorage() AccountRepository {
-	conn, err := pgx.Connect(context.Background(), "postgres://postgres:1234@localhost:5432/desafio")
-	if err != nil {
-		log.Printf("unable to connect to data base: %v", err)
-	}
-	return AccountRepository{conn}
+func NewStorage(connection *pgx.Pool) AccountRepository {
+	return AccountRepository{connection}
 }
