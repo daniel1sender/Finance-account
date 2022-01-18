15c433f135cde00cb63218f381904f54f57b68c4
Daniel Sender <daniel.saul7@gmail.com>
2022-01-18 18:50:47 -0300

    feat: add update balance test with db connetion

--- pkg/domain/accounts/usecases/update_balance_test.go
+++ pkg/domain/accounts/usecases/update_balance_test.go
@@ -1,23 +1,23 @@
 package usecases
 
 import (
+	"context"
 	"errors"
 	"testing"
 
 	accounts_usecase "github.com/daniel1sender/Desafio-API/pkg/domain/accounts"
 	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
-	"github.com/daniel1sender/Desafio-API/pkg/gateways/store/memory/accounts"
-	accounts_repository "github.com/daniel1sender/Desafio-API/pkg/gateways/store/repository/accounts"
+	accounts_repository "github.com/daniel1sender/Desafio-API/pkg/gateways/store/postgres/accounts"
 )
 
 func TestAccountUseCase_UpdateBalance(t *testing.T) {
 
 	t.Run("should return an account and null error when account was updated", func(t *testing.T) {
 
-		storageFiles := accounts_repository.NewStorage()
-		accountUseCase := NewUseCase(storageFiles)
+		repository := accounts_repository.NewStorage(Db)
+		accountUseCase := NewUseCase(repository)
 		name := "John Doe"
-		cpf := "11111111030"
+		cpf := "12345678010"
 		secret := "123"
 		balance := 10
 
@@ -26,7 +26,7 @@ func TestAccountUseCase_UpdateBalance(t *testing.T) {
 			t.Errorf("expected no error to create a new account but got '%s'", err)
 		}
 
-		storageFiles.Upsert(account)
+		repository.Upsert(account)
 
 		updateAccountError := accountUseCase.UpdateBalance(account.ID, 20.0)
 
@@ -38,12 +38,23 @@ func TestAccountUseCase_UpdateBalance(t *testing.T) {
 
 	t.Run("should return an empty account an error when account don't exists", func(t *testing.T) {
 
-		storage := accounts.NewStorage()
-		accountUseCase := NewUseCase(storage)
+		repository := accounts_repository.NewStorage(Db)
+		accountUseCase := NewUseCase(repository)
 
-		err := accountUseCase.UpdateBalance("1", 20.0)
+		name := "John Doe"
+		cpf := "11111111031"
+		secret := "123"
+		balance := 10
 
-		if err != accounts_usecase.ErrIDNotFound {
+		account, err := entities.NewAccount(name, cpf, secret, balance)
+		if err != nil {
+			t.Errorf("expected no error to create a new account but got '%s'", err)
+		}
+		repository.Exec(context.Background(), "DELETE FROM accounts")
+
+		err = accountUseCase.UpdateBalance(account.ID, 20.0)
+
+		if errors.Is(err, accounts_usecase.ErrIDNotFound) {
 			t.Errorf("expected '%s' but got '%s'", accounts_usecase.ErrIDNotFound, err)
 		}
 
@@ -51,11 +62,11 @@ func TestAccountUseCase_UpdateBalance(t *testing.T) {
 
 	t.Run("should return an empty account and an error when balance account is less than zero", func(t *testing.T) {
 
-		storageFiles := accounts_repository.NewStorage()
-		accountUseCase := NewUseCase(storageFiles)
+		repository := accounts_repository.NewStorage(Db)
+		accountUseCase := NewUseCase(repository)
 
 		name := "John Doe"
-		cpf := "11111111030"
+		cpf := "11111111039"
 		secret := "123"
 		balance := 10
 
@@ -64,7 +75,7 @@ func TestAccountUseCase_UpdateBalance(t *testing.T) {
 			t.Errorf("expected no error to create a new account but got '%s'", err)
 		}
 
-		storageFiles.Upsert(account)
+		repository.Upsert(account)
 
 		err = accountUseCase.UpdateBalance(account.ID, -10)
 
