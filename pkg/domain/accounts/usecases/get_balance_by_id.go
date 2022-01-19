01075bd05f14e6eaafe89e2457e818d0b8896ddd
Daniel Sender <daniel.saul7@gmail.com>
2022-01-19 18:41:37 -0300

    style: adjust error message

--- pkg/domain/accounts/usecases/get_balance_by_id.go
+++ pkg/domain/accounts/usecases/get_balance_by_id.go
@@ -5,7 +5,7 @@ import "fmt"
 func (au AccountUseCase) GetBalanceByID(id string) (int, error) {
 	balance, err := au.storage.GetBalanceByID(id)
 	if err != nil {
-		return balance, fmt.Errorf("error getting balance account: %w", err)
+		return balance, fmt.Errorf("error while getting balance account: %w", err)
 	}
 	return balance, err
 }
