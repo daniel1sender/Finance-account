b5fc548444149262c092d7fd9d858f62ac563144
Daniel Sender <daniel.saul7@gmail.com>
2022-01-19 18:56:27 -0300

    style: improve error handling

--- pkg/gateways/store/postgres/accounts/get_all.go
+++ pkg/gateways/store/postgres/accounts/get_all.go
@@ -2,26 +2,30 @@ package accounts
 
 import (
 	"context"
-	"log"
+	"fmt"
 
+	"github.com/daniel1sender/Desafio-API/pkg/domain/accounts"
 	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
 )
 
-func (ar AccountRepository) GetAll() []entities.Account {
+func (ar AccountRepository) GetAll() ([]entities.Account, error) {
 	var users []entities.Account
 	var user entities.Account
 
 	rows, err := ar.Query(context.Background(), "SELECT id, name, cpf, secret, balance, created_at FROM accounts")
 	if err != nil {
-		log.Printf("error to read table rows: %v", err)
+		return []entities.Account{}, fmt.Errorf("error to read table rows: %w", err)
 	}
 
 	for rows.Next() {
 		err := rows.Scan(&user.ID, &user.Name, &user.CPF, &user.Secret, &user.Balance, &user.CreatedAt)
 		if err != nil {
-			log.Print(err)
+			return []entities.Account{}, err
 		}
 		users = append(users, user)
 	}
-	return users
+	if len(users) == 0{
+		return []entities.Account{}, fmt.Errorf("error while listing accounts: %w", accounts.ErrEmptyList)
+	}
+	return users, nil
 }
--- pkg/gateways/store/postgres/accounts/get_by_id.go
+++ pkg/gateways/store/postgres/accounts/get_by_id.go
@@ -3,6 +3,7 @@ package accounts
 import (
 	"context"
 
+	"github.com/daniel1sender/Desafio-API/pkg/domain/accounts"
 	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
 )
 
@@ -10,7 +11,7 @@ func (ar AccountRepository) GetByID(id string) (entities.Account, error) {
 	account := entities.Account{}
 	err := ar.QueryRow(context.Background(), "SELECT id, name, cpf, secret, balance, created_at FROM accounts WHERE id = $1", id).Scan(&account.ID, &account.Name, &account.CPF, &account.Secret, &account.Balance, &account.CreatedAt)
 	if err != nil {
-		return entities.Account{}, err
+		return entities.Account{}, accounts.ErrIDNotFound
 	}
 
 	return account, nil
