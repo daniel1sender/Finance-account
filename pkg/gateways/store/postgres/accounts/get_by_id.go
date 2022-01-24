56256a3628b4dae62a7f829f061bf3c456ad4c25
Daniel Sender <daniel.saul7@gmail.com>
2022-01-24 12:48:34 -0300

    style: improve readability separing long strings

--- pkg/gateways/store/postgres/accounts/get_by_id.go
+++ pkg/gateways/store/postgres/accounts/get_by_id.go
@@ -6,9 +6,11 @@ import (
 	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
 )
 
+const GetByIDStatement = "SELECT id, name, cpf, secret, balance, created_at FROM accounts WHERE id = $1"
+
 func (ar AccountRepository) GetByID(id string) (entities.Account, error) {
 	account := entities.Account{}
-	err := ar.QueryRow(context.Background(), "SELECT id, name, cpf, secret, balance, created_at FROM accounts WHERE id = $1", id).Scan(&account.ID, &account.Name, &account.CPF, &account.Secret, &account.Balance, &account.CreatedAt)
+	err := ar.QueryRow(context.Background(), GetByIDStatement, id).Scan(&account.ID, &account.Name, &account.CPF, &account.Secret, &account.Balance, &account.CreatedAt)
 	if err != nil {
 		return entities.Account{}, err
 	}
