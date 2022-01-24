e619372ffddf91a4186a3073c3453aedf4e9ac76
Daniel Sender <daniel.saul7@gmail.com>
2022-01-24 12:07:18 -0300

    refactor: move error handling to storage layer to avoid coupling between storage and use case

--- pkg/domain/accounts/usecases/create.go
+++ pkg/domain/accounts/usecases/create.go
@@ -1,24 +1,15 @@
 package usecases
 
 import (
-	"errors"
 	"fmt"
 
-	"github.com/daniel1sender/Desafio-API/pkg/domain/accounts"
 	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
-	"github.com/jackc/pgx/v4"
 )
 
 func (au AccountUseCase) Create(name, cpf, secret string, balance int) (entities.Account, error) {
 
-	err := au.storage.CheckCPF(cpf)
-	if err != nil {
-		switch {
-		case errors.Is(err, accounts.ErrExistingCPF):
-			return entities.Account{}, fmt.Errorf("%w", accounts.ErrExistingCPF)
-		case !errors.Is(err, pgx.ErrNoRows):
-			return entities.Account{}, fmt.Errorf("%w", err)
-		}
+	if err := au.storage.CheckCPF(cpf); err != nil {
+		return entities.Account{}, fmt.Errorf("%w", err)
 	}
 
 	account, err := entities.NewAccount(name, cpf, secret, balance)
--- pkg/gateways/store/postgres/accounts/check_cpf.go
+++ pkg/gateways/store/postgres/accounts/check_cpf.go
@@ -2,15 +2,23 @@ package accounts
 
 import (
 	"context"
+	"errors"
 
 	"github.com/daniel1sender/Desafio-API/pkg/domain/accounts"
+	"github.com/jackc/pgx/v4"
 )
 
 func (ar AccountRepository) CheckCPF(cpf string) error {
 	var CPFaccount string
 	err := ar.QueryRow(context.Background(), "SELECT cpf FROM accounts WHERE cpf = $1", cpf).Scan(&CPFaccount)
 	if err != nil {
-		return err
+		switch {
+		case errors.Is(err, pgx.ErrNoRows):
+			return nil
+		default:
+			return err
+		}
+
 	}
 	return accounts.ErrExistingCPF
 }
