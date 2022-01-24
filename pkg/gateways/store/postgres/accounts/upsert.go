4bd2e6c79905216d05bea29376d04cf444a3e768
Daniel Sender <daniel.saul7@gmail.com>
2022-01-24 12:48:34 -0300

    style: improve readability separing long strings

--- pkg/gateways/store/postgres/accounts/upsert.go
+++ pkg/gateways/store/postgres/accounts/upsert.go
@@ -7,8 +7,25 @@ import (
 	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
 )
 
+const upsertStatement = `INSERT INTO accounts(
+	id, 
+	name, 
+	cpf, 
+	secret, 
+	balance, 
+	created_at
+	) VALUES (
+		$1, 
+		$2, 
+		$3, 
+		$4, 
+		$5, 
+		$6
+	) ON CONFLICT (id) DO UPDATE SET balance = 
+	EXCLUDED.balance`
+
 func (ar AccountRepository) Upsert(account entities.Account) error {
-	if _, err := ar.Exec(context.Background(), "INSERT INTO accounts(id, name, cpf, secret, balance, created_at) VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT (id) DO UPDATE SET balance = EXCLUDED.balance", account.ID, account.Name, account.CPF, account.Secret, account.Balance, account.CreatedAt); err != nil {
+	if _, err := ar.Exec(context.Background(), upsertStatement, account.ID, account.Name, account.CPF, account.Secret, account.Balance, account.CreatedAt); err != nil {
 		return fmt.Errorf("unable to insert the account due to: %v", err)
 	}
 	return nil
