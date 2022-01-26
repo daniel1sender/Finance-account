34ab3e1d0087738ffc0ee4539d54571671b0002a
Daniel Sender <daniel.saul7@gmail.com>
2022-01-26 13:59:45 -0300

    style: remove context errors to use case layer to separe context errors only in store layer

--- pkg/domain/accounts/usecases/get_balance_by_id.go
+++ pkg/domain/accounts/usecases/get_balance_by_id.go
@@ -1,15 +1,9 @@
 package usecases
 
-import (
-	"fmt"
-
-	"github.com/daniel1sender/Desafio-API/pkg/domain/accounts"
-)
-
 func (au AccountUseCase) GetBalanceByID(id string) (int, error) {
 	balance, err := au.storage.GetBalanceByID(id)
 	if err != nil {
-		return balance, fmt.Errorf("%v : %w", accounts.ErrAccountFound, err)
+		return balance, err
 	}
-	return balance, err
+	return balance, nil
 }
--- pkg/domain/accounts/usecases/get_by_id.go
+++ pkg/domain/accounts/usecases/get_by_id.go
@@ -1,16 +1,13 @@
 package usecases
 
 import (
-	"fmt"
-
-	"github.com/daniel1sender/Desafio-API/pkg/domain/accounts"
 	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
 )
 
 func (au AccountUseCase) GetByID(id string) (entities.Account, error) {
 	account, err := au.storage.GetByID(id)
 	if err != nil {
-		return entities.Account{}, fmt.Errorf("%w: %v", accounts.ErrAccountFound, err)
+		return entities.Account{}, err
 	}
 	return account, err
 }
