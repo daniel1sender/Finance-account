d5889664dac1a395361cd0f5eb6d4718410a5d40
Daniel Sender <daniel.saul7@gmail.com>
2022-01-26 14:02:56 -0300

    refactor: improve logic to search information in postgres storage

--- pkg/gateways/store/postgres/accounts/get_by_id.go
+++ pkg/gateways/store/postgres/accounts/get_by_id.go
@@ -3,7 +3,9 @@ package accounts
 import (
 	"context"
 
+	"github.com/daniel1sender/Desafio-API/pkg/domain/accounts"
 	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
+	"github.com/jackc/pgx/v4"
 )
 
 const GetByIDStatement = "SELECT id, name, cpf, secret, balance, created_at FROM accounts WHERE id = $1"
@@ -11,7 +13,9 @@ const GetByIDStatement = "SELECT id, name, cpf, secret, balance, created_at FROM
 func (ar AccountRepository) GetByID(id string) (entities.Account, error) {
 	account := entities.Account{}
 	err := ar.QueryRow(context.Background(), GetByIDStatement, id).Scan(&account.ID, &account.Name, &account.CPF, &account.Secret, &account.Balance, &account.CreatedAt)
-	if err != nil {
+	if err == pgx.ErrNoRows {
+		return entities.Account{}, accounts.ErrAccountNotFound
+	} else if err != nil {
 		return entities.Account{}, err
 	}
 
