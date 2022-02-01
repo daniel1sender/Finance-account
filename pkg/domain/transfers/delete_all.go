5b66b56d872ea9733bb76a6d8674063abecc8646
Daniel Sender <daniel.saul7@gmail.com>
2022-02-01 12:17:22 -0300

    feat: add new method to clean the transfers database

---
+++ pkg/domain/transfers/delete_all.go
@@ -0,0 +1,16 @@
+package transfers
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+func DeleteAll(Db *pgxpool.Pool) error {
+	_, err := Db.Exec(context.Background(), "DELETE FROM accounts")
+	if err != nil {
+		return fmt.Errorf("error while deleting table transfers: %w", err)
+	}
+	return nil
+}
