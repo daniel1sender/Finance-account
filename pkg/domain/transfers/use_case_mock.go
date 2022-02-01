f447766b32f9a245a6e7c0a5fc184c8ef19ddde2
Daniel Sender <daniel.saul7@gmail.com>
2022-02-01 12:12:51 -0300

    fix: move the use case to update balance from accounts domain to transfers domain

--- pkg/domain/transfers/use_case_mock.go
+++ pkg/domain/transfers/use_case_mock.go
@@ -1,12 +1,20 @@
 package transfers
 
-import "github.com/daniel1sender/Desafio-API/pkg/domain/entities"
+import (
+	"context"
+
+	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
+)
 
 type UseCaseMock struct {
 	Transfer entities.Transfer
 	Error    error
 }
 
-func (m *UseCaseMock) Make(originID, destinationID string, amount int) (entities.Transfer, error) {
+func (m *UseCaseMock) Make(ctx context.Context, originID, destinationID string, amount int) (entities.Transfer, error) {
 	return m.Transfer, m.Error
+}
+
+func (m *UseCaseMock)UpdateBalance(ctx context.Context, id string, balance int)error{
+	panic("not implemented")
 }
