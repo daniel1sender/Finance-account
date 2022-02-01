bea9476493d242d2bffd92373b5bdb39ec4489bb
Daniel Sender <daniel.saul7@gmail.com>
2022-02-01 12:06:02 -0300

    fix: change constraint of transfer migration to be in according with application business rule

--- pkg/gateways/store/postgres/migrations/000002_create_transfers_table.up.sql
+++ pkg/gateways/store/postgres/migrations/000002_create_transfers_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS transfers(
 id UUID PRIMARY KEY,
-account_origin_id UUID,
-account_destination_id UUID,
+account_origin_id TEXT NOT NULL,
+account_destination_id TEXT NOT NULL,
 amount BIGINT NOT NULL,
 created_at TIMESTAMP WITH TIME ZONE NOT NULL
 );
