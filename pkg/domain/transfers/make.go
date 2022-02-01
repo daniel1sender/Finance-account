9548b9510fd83c6c78f554f41f2e260e96968ac7
Daniel Sender <daniel.saul7@gmail.com>
2022-02-01 12:22:55 -0300

    fix: add context and refactoring tests

--- pkg/domain/transfers/make.go
+++ pkg/domain/transfers/make.go
@@ -1,40 +1,51 @@
 package transfers
 
 import (
+	"context"
 	"errors"
 	"fmt"
 
-	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
 	"github.com/daniel1sender/Desafio-API/pkg/domain/accounts"
+	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
 )
 
 var (
-	ErrInsufficientFunds     = errors.New("insufficient balance on account")
-	ErrOriginIDNotFound      = errors.New("transfer origin account id not found")
-	ErrDestinationIDNotFound = errors.New("transfer destination account id not found")
+	ErrInsufficientFunds          = errors.New("insufficient balance on account")
+	ErrOriginAccountNotFound      = errors.New("transfer origin account not found")
+	ErrDestinationAccountNotFound = errors.New("transfer destination account not found")
 )
 
-func (tu TransferUseCase) Make(originID, destinationID string, amount int) (entities.Transfer, error) {
+func (tu TransferUseCase) Make(ctx context.Context, originID, destinationID string, amount int) (entities.Transfer, error) {
 
-	originAccountBalance, err := tu.accountStorage.GetBalanceByID(originID)
+	originAccountBalance, err := tu.accountStorage.GetBalanceByID(ctx, originID)
 	if errors.Is(err, accounts.ErrAccountNotFound) {
-		return entities.Transfer{}, ErrOriginIDNotFound
+		return entities.Transfer{}, ErrOriginAccountNotFound
 	}
 	if originAccountBalance < amount {
 		return entities.Transfer{}, ErrInsufficientFunds
 	}
 
-	_, err = tu.accountStorage.GetByID(destinationID)
+	_, err = tu.accountStorage.GetByID(ctx, destinationID)
 	if errors.Is(err, accounts.ErrAccountNotFound) {
-		return entities.Transfer{}, ErrDestinationIDNotFound
+		return entities.Transfer{}, ErrDestinationAccountNotFound
 	}
 
 	transfer, err := entities.NewTransfer(originID, destinationID, amount)
 	if err != nil {
-		return entities.Transfer{}, fmt.Errorf("error creating a transfer: %w", err)
+		return entities.Transfer{}, fmt.Errorf("error while creating a transfer: %w", err)
 	}
 
-	err = tu.transferStorage.UpdateByID(transfer)
+	err = tu.transferStorage.UpdateByID(ctx, transfer)
+	if err != nil {
+		return entities.Transfer{}, err
+	}
+
+	err = tu.UpdateBalance(ctx, originID, -amount)
+	if err != nil {
+		return entities.Transfer{}, err
+	}
+	err = tu.UpdateBalance(ctx, destinationID, amount)
+
 	if err != nil {
 		return entities.Transfer{}, err
 	}
