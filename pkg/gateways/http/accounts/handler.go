b206eee827e640c2f333134c9348528ffb9db8a6
Daniel Sender <daniel.saul7@gmail.com>
2022-02-03 16:40:46 -0300

    feat: inject new logrus entry in accounts handler

--- pkg/gateways/http/accounts/handler.go
+++ pkg/gateways/http/accounts/handler.go
@@ -2,14 +2,17 @@ package accounts
 
 import (
 	"github.com/daniel1sender/Desafio-API/pkg/domain/accounts"
+	"github.com/sirupsen/logrus"
 )
 
 type Handler struct {
 	useCase accounts.UseCase
+	logger  *logrus.Entry
 }
 
-func NewHandler(useCase accounts.UseCase) Handler {
+func NewHandler(useCase accounts.UseCase, logger *logrus.Entry) Handler {
 	return Handler{
 		useCase: useCase,
+		logger:  logger,
 	}
 }
