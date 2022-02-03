9ca10321a654cabdf7eaaa85fafa8281a4a6822c
Daniel Sender <daniel.saul7@gmail.com>
2022-02-03 20:23:45 -0300

    fix: correct query sql in deleteAll method

--- pkg/domain/transfers/delete_all.go
+++ pkg/domain/transfers/delete_all.go
@@ -8,7 +8,7 @@ import (
 )
 
 func DeleteAll(Db *pgxpool.Pool) error {
-	_, err := Db.Exec(context.Background(), "DELETE FROM accounts")
+	_, err := Db.Exec(context.Background(), "DELETE FROM transfers")
 	if err != nil {
 		return fmt.Errorf("error while deleting table transfers: %w", err)
 	}
