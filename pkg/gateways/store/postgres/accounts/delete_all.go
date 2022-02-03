7f32b94b6072ed15131eec6aa363d40fdb4dfe3f
Daniel Sender <daniel.saul7@gmail.com>
2022-02-03 15:28:44 -0300

    style: improve sql query formatting of delete all function

--- pkg/gateways/store/postgres/accounts/delete_all.go
+++ pkg/gateways/store/postgres/accounts/delete_all.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 )
 
+const deleteAllStatement = `DELETE FROM accounts`
+
 func (ar AccountRepository) DeleteAll(ctx context.Context) error {
-	_, err := ar.Exec(ctx, "DELETE FROM accounts")
-	if err != nil{
+	_, err := ar.Exec(ctx, deleteAllStatement)
+	if err != nil {
 		return fmt.Errorf("error while deleting accounts: %w", err)
 	}
 	return nil
