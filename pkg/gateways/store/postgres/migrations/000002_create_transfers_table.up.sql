5925c745722cdc4b9edb9410bf7391c42dc5a03d
Daniel Sender <daniel.saul7@gmail.com>
2022-02-04 15:01:56 -0300

    refator: change the type of id in transfers to ensure the aplciation bussiness rule

--- pkg/gateways/store/postgres/migrations/000002_create_transfers_table.up.sql
+++ pkg/gateways/store/postgres/migrations/000002_create_transfers_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS transfers(
 id UUID PRIMARY KEY,
-account_origin_id TEXT NOT NULL,
-account_destination_id TEXT NOT NULL,
+account_origin_id UUID NOT NULL,
+account_destination_id UUID NOT NULL,
 amount BIGINT NOT NULL,
 created_at TIMESTAMP WITH TIME ZONE NOT NULL
 );
