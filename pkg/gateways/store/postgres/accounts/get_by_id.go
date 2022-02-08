8efeb0184528744418282ad56379cef065cf3576
Daniel Sender <daniel.saul7@gmail.com>
2022-02-08 10:49:21 -0300

    style: correct the name of statement according the scope variables

--- pkg/gateways/store/postgres/accounts/get_by_id.go
+++ pkg/gateways/store/postgres/accounts/get_by_id.go
@@ -8,11 +8,11 @@ import (
 	"github.com/jackc/pgx/v4"
 )
 
-const GetByIDStatement = "SELECT id, name, cpf, secret, balance, created_at FROM accounts WHERE id = $1"
+const getByIDStatement = "SELECT id, name, cpf, secret, balance, created_at FROM accounts WHERE id = $1"
 
 func (ar AccountRepository) GetByID(ctx context.Context, id string) (entities.Account, error) {
 	account := entities.Account{}
-	err := ar.QueryRow(ctx, GetByIDStatement, id).Scan(&account.ID, &account.Name, &account.CPF, &account.Secret, &account.Balance, &account.CreatedAt)
+	err := ar.QueryRow(ctx, getByIDStatement, id).Scan(&account.ID, &account.Name, &account.CPF, &account.Secret, &account.Balance, &account.CreatedAt)
 	if err == pgx.ErrNoRows {
 		return entities.Account{}, accounts.ErrAccountNotFound
 	} else if err != nil {
