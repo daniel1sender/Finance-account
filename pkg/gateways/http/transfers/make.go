4d0b3da31b2d157649885ca858ac490e5c16fc11
Daniel Sender <daniel.saul7@gmail.com>
2022-02-09 13:10:13 -0300

    refactor: add logger in make use case to show more information about transfers

--- pkg/gateways/http/transfers/make.go
+++ pkg/gateways/http/transfers/make.go
@@ -3,12 +3,12 @@ package transfers
 import (
 	"encoding/json"
 	"errors"
-	"log"
 	"net/http"
 
 	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
 	"github.com/daniel1sender/Desafio-API/pkg/domain/transfers"
 	server_http "github.com/daniel1sender/Desafio-API/pkg/gateways/http"
+	"github.com/sirupsen/logrus"
 )
 
 type Request struct {
@@ -27,12 +27,13 @@ type Response struct {
 
 func (h Handler) Make(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
+	log := h.logger
 	var createRequest Request
 	err := json.NewDecoder(r.Body).Decode(&createRequest)
 	if err != nil {
 		w.Header().Add("Content-Type", server_http.JSONContentType)
 		response := server_http.Error{Reason: "invalid request body"}
-		log.Printf("error decoding body: %s\n", err)
+		log.WithError(err).Error("error decoding body")
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(response)
 		return
@@ -41,7 +42,7 @@ func (h Handler) Make(w http.ResponseWriter, r *http.Request) {
 	transfer, err := h.useCase.Make(ctx, createRequest.AccountOriginID, createRequest.AccountDestinationID, createRequest.Amount)
 	w.Header().Add("Content-Type", server_http.JSONContentType)
 	if err != nil {
-		log.Printf("create transfer request failed: %s\n", err.Error())
+		log.WithError(err).Error("create transfer request failed")
 		switch {
 
 		case errors.Is(err, transfers.ErrOriginAccountNotFound):
@@ -81,5 +82,8 @@ func (h Handler) Make(w http.ResponseWriter, r *http.Request) {
 	response := Response{transfer.ID, transfer.AccountOriginID, transfer.AccountDestinationID, transfer.Amount, ExpectedCreateAt}
 	w.WriteHeader(http.StatusCreated)
 	_ = json.NewEncoder(w).Encode(response)
-
+	log.WithFields(logrus.Fields{
+		"origin_account_id":      transfer.AccountOriginID,
+		"destination_account_id": transfer.AccountOriginID,
+	}).Info("transfer successful")
 }
