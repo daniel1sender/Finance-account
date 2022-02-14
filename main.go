e96d374e393a338f6b354b88cba4b5329a54b087
Daniel Sender <daniel.saul7@gmail.com>
2022-02-14 17:30:29 -0300

    style: improve variable name according the camel case practice

--- main.go
+++ main.go
@@ -27,18 +27,18 @@ func main() {
 	log := logrus.New()
 	log.SetFormatter(&logrus.JSONFormatter{})
 	entry := logrus.NewEntry(log)
-	var apiconfig Config
-	err := envconfig.Process("", &apiconfig)
+	var apiConfig Config
+	err := envconfig.Process("", &apiConfig)
 	if err != nil {
 		log.WithError(err).Fatal("error while processing environment variables")
 	}
 
-	err = postgres.RunMigrations(apiconfig.DatabaseURL)
+	err = postgres.RunMigrations(apiConfig.DatabaseURL)
 	if err != nil {
 		log.Fatalf("error to run migrations: %v", err)
 	}
 
-	dbPool, err := pgxpool.Connect(context.Background(), apiconfig.DatabaseURL)
+	dbPool, err := pgxpool.Connect(context.Background(), apiConfig.DatabaseURL)
 	if err != nil {
 		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
@@ -59,7 +59,7 @@ func main() {
 	r.HandleFunc("/accounts/{id}/balance", accountHandler.GetBalanceByID).Methods(http.MethodGet)
 	r.HandleFunc("/transfers", transferHandler.Make).Methods(http.MethodPost)
 
-	if err := http.ListenAndServe(apiconfig.Port, r); err != nil {
+	if err := http.ListenAndServe(apiConfig.Port, r); err != nil {
 		log.Fatalf("failed to listen and serve: %s", err)
 	}
 }
