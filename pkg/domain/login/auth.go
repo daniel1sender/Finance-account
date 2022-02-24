d70fef176d35ddad473a1ed70ff5b4c4869ed60f
Daniel Sender <daniel.saul7@gmail.com>
2022-02-24 18:17:02 -0300

    feat: implement login use-case to authenticate account

--- pkg/domain/login/auth.go
+++ pkg/domain/login/auth.go
@@ -1,18 +1,43 @@
 package login
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
-	jwt "github.com/dgrijalva/jwt-go"
+	jwt "github.com/golang-jwt/jwt/v4"
+	"golang.org/x/crypto/bcrypt"
 )
 
-func GenerateJWT(id string) (string, error) {
-	claim := entities.NewClaim(id)
-	JWTtoken := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
-	token, err := JWTtoken.SignedString(JWTtoken)
+func (l LoginUseCase) Auth(ctx context.Context, cpf, secret string) (string, error) {
+	account, err := l.AccountStorage.GetByCPF(ctx, cpf)
 	if err != nil {
-		return "", fmt.Errorf("error to get the signed token: %w", err)
+		return "", fmt.Errorf("error while getting account by cpf: %w", err)
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(account.Secret), []byte(secret))
+	if err != nil {
+		return "", fmt.Errorf("error while validate secret: %w", err)
+	}
+
+	token, err := GenerateJWT(account.ID, l.tokenSecret)
+	if err != nil {
+		return "", fmt.Errorf("error while generating token: %w", err)
+	}
+	return token, nil
+}
+
+func GenerateJWT(accountID string, tokenSecret string) (string, error) {
+	claim := entities.NewClaim(accountID)
+	claims := jwt.RegisteredClaims{
+		Subject:   accountID,
+		ExpiresAt: jwt.NewNumericDate(claim.ExpTime),
+		IssuedAt:  jwt.NewNumericDate(claim.CreatedTime),
+		ID:        claim.TokenID,
+	}
+	tokenJWT := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := tokenJWT.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", fmt.Errorf("error while getting the signed token: %w", err)
 	}
 	return token, nil
 }
