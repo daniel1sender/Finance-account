8c5aee66ac969b1ac8f1bd8a5b345c5304422714
Daniel Sender <daniel.saul7@gmail.com>
2022-02-24 18:20:33 -0300

    test: use dockertest library to start postgres

---
+++ pkg/domain/login/postgres_test.go
@@ -0,0 +1,74 @@
+package login
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/daniel1sender/Desafio-API/pkg/gateways/store/postgres"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	pgx "github.com/jackc/pgx/v4/pgxpool"
+	_ "github.com/lib/pq"
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
+)
+
+var Db *pgx.Pool
+
+func TestMain(m *testing.M) {
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "11",
+		Env: []string{
+			"POSTGRES_PASSWORD=1234",
+			"POSTGRES_USER=postgres",
+			"POSTGRES_DB=test",
+			"listen_addresses = '*'",
+		},
+	}, func(config *docker.HostConfig) {
+		config.AutoRemove = true
+		config.RestartPolicy = docker.RestartPolicy{Name: "no"}
+	})
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	hostAndPort := resource.GetHostPort("5432/tcp")
+	databaseUrl := fmt.Sprintf("postgres://postgres:1234@%s/test?sslmode=disable", hostAndPort)
+
+	log.Println("Connecting to database on url: ", databaseUrl)
+
+	resource.Expire(30)
+
+	pool.MaxWait = 30 * time.Second
+	if err = pool.Retry(func() error {
+		Db, err = pgx.Connect(context.Background(), databaseUrl)
+		if err != nil {
+			return err
+		}
+		return Db.Ping(context.Background())
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	err = postgres.RunMigrations(databaseUrl)
+	if err != nil {
+		log.Fatalf("error to run migrations: %v", err)
+	}
+
+	code := m.Run()
+
+	if err := pool.Purge(resource); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
+
+	os.Exit(code)
+}
