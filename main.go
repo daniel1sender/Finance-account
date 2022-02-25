d276a7b0917447c0a77fbc8ea6b24335b9224561
Daniel Sender <daniel.saul7@gmail.com>
2022-02-25 15:58:10 -0300

    refactor: set enviroment variable to represent the token secret

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ import (
 type Config struct {
 	DatabaseURL string `envconfig:"DB_URL" required:"true"`
 	Port        string `envconfig:"API_PORT" required:"true" default:":3000"`
+	TokenSecret string `envconfig:"TOKEN_SECRET" required:"true"`
 }
 
 func main() {
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #%/bin/bash
 export DB_URL="postgres://postgres:1234@localhost:5432/desafio"
 export API_PORT=":3000"
+export TOKEN_SECRET="123"
 go run main.go
