26666ec826dc8a98ac386b1f6e4e22822a30078d
Daniel Sender <daniel.saul7@gmail.com>
2022-03-02 15:47:33 -0300

    refactor: edit login data table to improve readability

--- pkg/gateways/store/postgres/login/insert.go
+++ pkg/gateways/store/postgres/login/insert.go
@@ -9,22 +9,24 @@ import (
 
 const insertStatement = `INSERT INTO tokens(
 	id,
-	token,
+	sub,
+	exp_time,
 	created_at
 	) VALUES (
 	$1, 
 	$2, 
-	$3)`
+	$3,
+	$4)`
 
 func (l LoginRepository) Insert(ctx context.Context, token, tokenSecret string) error {
 	tokenParsed, err := jwt.ParseWithClaims(token, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte(tokenSecret), nil
 	})
 	if err != nil {
-		return fmt.Errorf("error while parsing token occurred %w", err)
+		return fmt.Errorf("error while parsing token occurred: %w", err)
 	}
 	claims := tokenParsed.Claims.(*jwt.RegisteredClaims)
-	if _, err := l.Exec(ctx, insertStatement, claims.ID, token, claims.IssuedAt.Time); err != nil {
+	if _, err := l.Exec(ctx, insertStatement, claims.ID, claims.Subject, claims.ExpiresAt.Time, claims.IssuedAt.Time); err != nil {
 		return fmt.Errorf("unable to insert the token due to: %v", err)
 	}
 	return nil
