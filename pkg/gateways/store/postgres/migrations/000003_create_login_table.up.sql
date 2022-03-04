8f29d918199309fdbcb885748a7cf0b45a3fd730
Daniel Sender <daniel.saul7@gmail.com>
2022-03-04 12:30:08 -0300

    refactor: adjust sub constraint

--- pkg/gateways/store/postgres/migrations/000003_create_login_table.up.sql
+++ pkg/gateways/store/postgres/migrations/000003_create_login_table.up.sql
@@ -1,7 +1,7 @@
 /*migration up of table tokens*/
 CREATE TABLE IF NOT EXISTS tokens(
     id UUID PRIMARY KEY,
-    sub TEXT UNIQUE NOT NULL,
+    sub TEXT NOT NULL,
     exp_time TIMESTAMP WITH TIME ZONE NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL
 );
