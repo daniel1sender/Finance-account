bc2312fa1e9a3a8a480d8e099a83bb79973a7af2
Daniel Sender <daniel.saul7@gmail.com>
2022-03-07 16:57:39 -0300

    feat: add usecase abstraction

--- pkg/domain/login/use_case.go
+++ pkg/domain/login/use_case.go
@@ -7,21 +7,31 @@ import (
 )
 
 type LoginUseCase struct {
+	LoginStorage   Repository
 	AccountStorage AccountRepository
 	tokenSecret    string
 }
 
 type UseCase interface {
-	Auth(ctx context.Context, cpf, secret string) (string, error)
+	Auth(ctx context.Context, cpf, secret string) (string, string, error)
+	CheckToken(ctx context.Context, token string) error
+	GetTokenByID(ctx context.Context, id string) (string, error)
 }
 
 type AccountRepository interface {
 	GetByCPF(ctx context.Context, cpf string) (entities.Account, error)
 }
 
-func NewUseCase(accountStorage AccountRepository, tokenSecret string) LoginUseCase {
+func NewUseCase(loginStorage Repository, accountStorage AccountRepository, tokenSecret string) LoginUseCase {
 	return LoginUseCase{
+		LoginStorage:   loginStorage,
 		AccountStorage: accountStorage,
 		tokenSecret:    tokenSecret,
 	}
+}
+
+type Repository interface {
+	CheckToken(ctx context.Context, token string) error
+	Insert(ctx context.Context, token, tokenSecret string) error
+	GetTokenByID(ctx context.Context, id string) (string, error)
 }
