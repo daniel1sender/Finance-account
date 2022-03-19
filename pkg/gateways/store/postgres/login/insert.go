6f4eec943456232fc8dd638c6e14b2548dca0007
Daniel Sender <daniel.saul7@gmail.com>
2022-03-19 19:00:06 -0300

    feat: add insertion method for login database

---
+++ pkg/gateways/store/postgres/login/insert.go
@@ -0,0 +1,35 @@
+package login
+
+import (
+	"context"
+	"fmt"
+
+	jwt "github.com/golang-jwt/jwt/v4"
+)
+
+const insertStatement = `INSERT INTO tokens(
+	id,
+	sub,
+	exp_time,
+	created_at,
+	token
+	) VALUES (
+	$1, 
+	$2, 
+	$3,
+	$4,
+	$5)`
+
+func (l LoginRepository) Insert(ctx context.Context, token, tokenSecret string) error {
+	tokenParsed, err := jwt.ParseWithClaims(token, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return fmt.Errorf("error while parsing token occurred: %w", err)
+	}
+	claims := tokenParsed.Claims.(*jwt.RegisteredClaims)
+	if _, err := l.Exec(ctx, insertStatement, claims.ID, claims.Subject, claims.ExpiresAt.Time, claims.IssuedAt.Time, token); err != nil {
+		return fmt.Errorf("unable to insert the token due to: %v", err)
+	}
+	return nil
+} 
