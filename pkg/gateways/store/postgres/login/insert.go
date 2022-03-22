9cb62ca2bb8b33a019aab0c8c147ee0a58edb7e9
Daniel Sender <daniel.saul7@gmail.com>
2022-03-22 10:56:43 -0300

    refactor: replace get token by account id to token id

--- pkg/gateways/store/postgres/login/insert.go
+++ pkg/gateways/store/postgres/login/insert.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	jwt "github.com/golang-jwt/jwt/v4"
+	"github.com/daniel1sender/Desafio-API/pkg/domain/login"
 )
 
 const insertStatement = `INSERT INTO tokens(
@@ -21,15 +21,12 @@ const insertStatement = `INSERT INTO tokens(
 	$5)`
 
 func (l LoginRepository) Insert(ctx context.Context, token, tokenSecret string) error {
-	tokenParsed, err := jwt.ParseWithClaims(token, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
-		return []byte(tokenSecret), nil
-	})
+	claims, err := login.ParseToken(token, tokenSecret)
 	if err != nil {
 		return fmt.Errorf("error while parsing token occurred: %w", err)
 	}
-	claims := tokenParsed.Claims.(*jwt.RegisteredClaims)
 	if _, err := l.Exec(ctx, insertStatement, claims.ID, claims.Subject, claims.ExpiresAt.Time, claims.IssuedAt.Time, token); err != nil {
-		return fmt.Errorf("unable to insert the token due to: %v", err)
+		return fmt.Errorf("unable to insert the token due to: %w", err)
 	}
 	return nil
-} 
+}
