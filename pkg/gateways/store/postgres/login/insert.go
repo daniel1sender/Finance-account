603dd39d89fa8f2070b711173e28e1325393cd68
Daniel Sender <daniel.saul7@gmail.com>
2022-03-22 12:05:59 -0300

    refactor: separe parsing token from repository

--- pkg/gateways/store/postgres/login/insert.go
+++ pkg/gateways/store/postgres/login/insert.go
@@ -3,8 +3,7 @@ package login
 import (
 	"context"
 	"fmt"
-
-	"github.com/daniel1sender/Desafio-API/pkg/domain/login"
+	"time"
 )
 
 const insertStatement = `INSERT INTO tokens(
@@ -20,12 +19,8 @@ const insertStatement = `INSERT INTO tokens(
 	$4,
 	$5)`
 
-func (l LoginRepository) Insert(ctx context.Context, token, tokenSecret string) error {
-	claims, err := login.ParseToken(token, tokenSecret)
-	if err != nil {
-		return fmt.Errorf("error while parsing token occurred: %w", err)
-	}
-	if _, err := l.Exec(ctx, insertStatement, claims.ID, claims.Subject, claims.ExpiresAt.Time, claims.IssuedAt.Time, token); err != nil {
+func (l LoginRepository) Insert(ctx context.Context, tokenID, accountID, token string, expiresAt, createdAt time.Time) error {
+	if _, err := l.Exec(ctx, insertStatement, tokenID, accountID, expiresAt, createdAt, token); err != nil {
 		return fmt.Errorf("unable to insert the token due to: %w", err)
 	}
 	return nil
