9ec0767c8aafdb16a25cca0c85b42c48038a77a3
Daniel Sender <daniel.saul7@gmail.com>
2022-03-25 17:16:23 -0300

    chore: change methods to usecases package

--- pkg/domain/login/usecases/get_token_by_id.go
+++
@@ -1,7 +0,0 @@
-package usecases
-
-import "context"
-
-func (l LoginUseCase) GetTokenByID(ctx context.Context, tokenID string) (string, error){
-	return l.LoginRepository.GetTokenByID(ctx, tokenID)
-}
