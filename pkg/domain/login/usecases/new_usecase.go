6b81a1c49e2e757abb711e373f273c8236ca78da
Daniel Sender <daniel.saul7@gmail.com>
2022-03-25 17:16:43 -0300

    style: delete code redundancies

--- pkg/domain/login/usecases/new_usecase.go
+++ pkg/domain/login/usecases/new_usecase.go
@@ -4,11 +4,11 @@ import "github.com/daniel1sender/Desafio-API/pkg/domain/login"
 
 type LoginUseCase struct {
 	AccountStorage  login.AccountRepository
-	LoginRepository login.LoginRepository
+	LoginRepository login.Repository
 	tokenSecret     string
 }
 
-func NewUseCase(accountStorage login.AccountRepository, loginRepository login.LoginRepository, tokenSecret string) LoginUseCase {
+func NewUseCase(accountStorage login.AccountRepository, loginRepository login.Repository, tokenSecret string) LoginUseCase {
 	return LoginUseCase{
 		AccountStorage:  accountStorage,
 		LoginRepository: loginRepository,
