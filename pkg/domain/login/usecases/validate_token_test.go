582c6aa4e76afa4a71461c2f7a012eea1d747839
Daniel Sender <daniel.saul7@gmail.com>
2022-03-25 17:23:38 -0300

    feat: add validation token use case

---
+++ pkg/domain/login/usecases/validate_token_test.go
@@ -0,0 +1,75 @@
+package usecases
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
+	"github.com/daniel1sender/Desafio-API/pkg/gateways/store/postgres/accounts"
+	"github.com/daniel1sender/Desafio-API/pkg/gateways/store/postgres/login"
+	"github.com/daniel1sender/Desafio-API/pkg/tests"
+	jwt "github.com/golang-jwt/jwt/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoginUsecase_ValidateToken(t *testing.T) {
+	ctx := context.Background()
+	accountRepository := accounts.NewStorage(Db)
+	loginRepository := login.NewStorage(Db)
+	tokenSecret := "123"
+	useCase := LoginUseCase{accountRepository, loginRepository, tokenSecret}
+	name := "Jonh Doe"
+	cpf := "01481623559"
+	accountSecret := "123"
+	balance := 10
+	account, _ := entities.NewAccount(name, cpf, accountSecret, balance)
+	duration := "1m"
+	expTime, _ := time.ParseDuration(duration)
+	claim := entities.NewClaim(account.ID, expTime)
+	claims := jwt.RegisteredClaims{
+		Subject:   account.ID,
+		ExpiresAt: jwt.NewNumericDate(claim.ExpTime),
+		IssuedAt:  jwt.NewNumericDate(claim.CreatedTime),
+		ID:        claim.TokenID,
+	}
+	tokenString, _ := GenerateJWT(claims, tokenSecret)
+
+	t.Run("should return a token succesfully", func(t *testing.T) {
+		loginRepository.Insert(ctx, claim, tokenString)
+		token, err := useCase.ValidateToken(ctx, tokenString)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, token)
+	})
+
+	t.Run("should return an empty token and an error when token is not found", func(t *testing.T) {
+		tests.DeleteAllTokens(Db)
+		token, err := useCase.ValidateToken(ctx, tokenString)
+		assert.Error(t, err)
+		assert.Empty(t, token)
+	})
+
+	t.Run("should return an empty token and an error when the token was expired", func(t *testing.T) {
+		duration := "1ms"
+		expTime, _ := time.ParseDuration(duration)
+		claim := entities.NewClaim(account.ID, expTime)
+		claims := jwt.RegisteredClaims{
+			Subject:   account.ID,
+			ExpiresAt: jwt.NewNumericDate(claim.ExpTime),
+			IssuedAt:  jwt.NewNumericDate(claim.CreatedTime),
+			ID:        claim.TokenID,
+		}
+		tokenString, _ := GenerateJWT(claims, tokenSecret)
+		loginRepository.Insert(ctx, claim, tokenString)
+		token, err := useCase.ValidateToken(ctx, tokenString)
+		assert.Error(t, err)
+		assert.Empty(t, token)
+	})
+
+	t.Run("should return an empty token and an error because token signature method is invalid", func(t *testing.T) {
+		tokenString := "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.tyh-VfuzIxCyGYDlkBA7DfyjrqmSHu6pQ2hoZuFqUSLPNY2N0mpHb3nk5K17HWP_3cYHBw7AhHale5wky6-sVA"
+		token, err := useCase.ValidateToken(ctx, tokenString)
+		assert.Error(t, err)
+		assert.Empty(t, token)
+	})
+}
