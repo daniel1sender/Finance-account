0e688f987188a681e08eef6dc289278e0af9b244
Daniel Sender <daniel.saul7@gmail.com>
2022-03-31 12:16:05 -0300

    refactor: supplement error handling with new checks

--- pkg/domain/login/usecases/login.go
+++ pkg/domain/login/usecases/login.go
@@ -5,18 +5,25 @@ import (
 	"fmt"
 
 	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
+	"github.com/daniel1sender/Desafio-API/pkg/domain/login"
 	jwt "github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func (l LoginUseCase) Login(ctx context.Context, cpf, accountSecret string) (string, error) {
+	if len(accountSecret) == 0 {
+		return "", fmt.Errorf("error while validating secret informed: %w", login.ErrEmptySecret)
+	}
+	if len(cpf) != 11 {
+		return "", fmt.Errorf("error while validanting cpf informed: %w", login.ErrInvalidCPF)
+	}
 	account, err := l.AccountStorage.GetByCPF(ctx, cpf)
 	if err != nil {
 		return "", fmt.Errorf("error while getting account by cpf: %w", err)
 	}
 	err = bcrypt.CompareHashAndPassword([]byte(account.Secret), []byte(accountSecret))
 	if err != nil {
-		return "", fmt.Errorf("error while validating secret: %w", err)
+		return "", fmt.Errorf("error while comparing secret informed with stored: %w", login.ErrInvalidSecret)
 	}
 
 	claim := entities.NewClaim(account.ID, l.expTime)
