fb8ec017c6c23eb4faed17036c4e7b8f789f736c
Daniel Sender <daniel.saul7@gmail.com>
2022-04-06 14:10:45 -0300

    feat: add use-case to use in http layer

--- pkg/domain/login/usecase.go
+++ pkg/domain/login/usecase.go
@@ -3,17 +3,20 @@ package login
 import (
 	"context"
 	"errors"
+
+	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
 )
 
 var (
-	ErrTokenNotFound = errors.New("token not found")
-	ErrInvalidToken  = errors.New("invalid token found")
-	ErrEmptySecret   = errors.New("empty secret was informed")
-	ErrInvalidCPF    = errors.New("cpf informed is invalid")
-	ErrInvalidSecret = errors.New("secret informed is incorrect")
-	ErrInvalidCredentials= errors.New("invalid credentials")
+	ErrTokenNotFound      = errors.New("token not found")
+	ErrInvalidToken       = errors.New("invalid token found")
+	ErrEmptySecret        = errors.New("empty secret was informed")
+	ErrInvalidCPF         = errors.New("cpf informed is invalid")
+	ErrInvalidSecret      = errors.New("secret informed is incorrect")
+	ErrInvalidCredentials = errors.New("invalid credentials")
 )
 
 type UseCase interface {
 	Login(ctx context.Context, cpf, accountSecret string) (string, error)
+	ValidateToken(ctx context.Context, tokenString string) (entities.Claims, error)
 }
