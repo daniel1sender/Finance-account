11b98f3229a73ed6a58a47e6c3b16872d24cab1f
Daniel Sender <daniel.saul7@gmail.com>
2022-04-11 08:34:24 -0300

    feat: create errors to use in repository

--- pkg/domain/transfers/usecase.go
+++ pkg/domain/transfers/usecase.go
@@ -2,8 +2,14 @@ package transfers
 
 import (
 	"context"
+	"errors"
 
 	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
+)
+
+var (
+	ErrEmptyList        = errors.New("empty list of transfers")
+	ErrTransferNotFound = errors.New("transfer not found")
 )
 
 type UseCase interface {
