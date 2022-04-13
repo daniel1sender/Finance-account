1d2338fede4cdf83e6a26e63763fe6a2dba31ef0
Daniel Sender <daniel.saul7@gmail.com>
2022-04-13 16:05:34 -0300

    feat: add errors and methods to use in http layer

--- pkg/domain/transfers/use_case_mock.go
+++ pkg/domain/transfers/use_case_mock.go
@@ -7,14 +7,19 @@ import (
 )
 
 type UseCaseMock struct {
-	Transfer entities.Transfer
-	Error    error
+	Transfer      entities.Transfer
+	Error         error
+	ListTransfers []entities.Transfer
 }
 
 func (m *UseCaseMock) Make(ctx context.Context, originID, destinationID string, amount int) (entities.Transfer, error) {
 	return m.Transfer, m.Error
 }
 
-func (m *UseCaseMock)UpdateBalance(ctx context.Context, id string, balance int)error{
+func (m *UseCaseMock) UpdateBalance(ctx context.Context, id string, balance int) error {
 	panic("not implemented")
+}
+
+func (m *UseCaseMock) ListByID(ctx context.Context, accountID string) ([]entities.Transfer, error) {
+	return m.ListTransfers, m.Error
 }
