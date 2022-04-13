5b2d2ee2dc8a57e47fdd8f77285372cefc91e5fe
Daniel Sender <daniel.saul7@gmail.com>
2022-04-13 16:05:56 -0300

    feat: add route to list transfer from account id

---
+++ pkg/gateways/http/transfers/list_by_id.go
@@ -0,0 +1,59 @@
+package transfers
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/daniel1sender/Desafio-API/pkg/domain/transfers"
+	server_http "github.com/daniel1sender/Desafio-API/pkg/gateways/http"
+	"github.com/sirupsen/logrus"
+)
+
+type ResponseList struct {
+	TransferID           string `json:"id"`
+	AccountOriginID      string `json:"account_origin_id"`
+	AccountDestinationID string `json:"account_destination_id"`
+	Amount               int    `json:"amount"`
+	CreatedAt            string `json:"create_at"`
+}
+
+func (h Handler) ListByID(w http.ResponseWriter, r *http.Request) {
+
+	ctx := r.Context()
+	originAccountID := ctx.Value(server_http.ContextAccountID).(string)
+	log := h.logger
+
+	transfersList, err := h.useCase.ListByID(ctx, originAccountID)
+	w.Header().Add("Content-Type", server_http.JSONContentType)
+	if err != nil {
+		log.WithError(err).Error("transfers listing request failed")
+		switch {
+		case errors.Is(err, transfers.ErrEmptyList):
+			response := server_http.Error{Reason: transfers.ErrTransfersNotFound.Error()}
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(response)
+		default:
+			response := server_http.Error{Reason: err.Error()}
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(response)
+		}
+		return
+	}
+
+	response := make([]ResponseList, len(transfersList))
+	for index, transfer := range transfersList {
+		response[index].TransferID = transfer.ID
+		response[index].AccountOriginID = transfer.AccountOriginID
+		response[index].AccountDestinationID = transfer.AccountDestinationID
+		response[index].Amount = transfer.Amount
+		response[index].CreatedAt = transfer.CreatedAt.String()
+	}
+
+	w.WriteHeader(http.StatusOK)
+	log.WithFields(logrus.Fields{
+		"origin_account_id": originAccountID,
+		"total_transfers":   len(transfersList),
+	}).Info("transfers listed successfully")
+	_ = json.NewEncoder(w).Encode(response)
+}
---
+++ pkg/gateways/http/transfers/list_by_id_test.go
@@ -0,0 +1,72 @@
+package transfers
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/daniel1sender/Desafio-API/pkg/domain/entities"
+	"github.com/daniel1sender/Desafio-API/pkg/domain/transfers"
+	server_http "github.com/daniel1sender/Desafio-API/pkg/gateways/http"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandlerListBy(t *testing.T) {
+	log := logrus.NewEntry(logrus.New())
+
+	t.Run("should return 200 and the list of transfers", func(t *testing.T) {
+		transfer := entities.Transfer{AccountOriginID: "1", AccountDestinationID: "0", Amount: 10}
+		transferList := make([]entities.Transfer, 0)
+		transferList = append(transferList, transfer)
+		useCase := transfers.UseCaseMock{ListTransfers: transferList}
+		h := NewHandler(&useCase, log)
+		newRequest, _ := http.NewRequest(http.MethodGet, "/transfers", nil)
+		ctx := context.WithValue(newRequest.Context(), server_http.ContextAccountID, transfer.AccountOriginID)
+		newResponse := httptest.NewRecorder()
+		h.ListByID(newResponse, newRequest.WithContext(ctx))
+
+		var response []ResponseList
+		_ = json.Unmarshal(newResponse.Body.Bytes(), &response)
+
+		assert.Equal(t, newResponse.Header().Get("content-type"), server_http.JSONContentType)
+		assert.Equal(t, newResponse.Code, http.StatusOK)
+		assert.NotEmpty(t, response)
+	})
+
+	t.Run("should return 404 and an error message when there are no transfers from that account", func(t *testing.T) {
+		useCase := transfers.UseCaseMock{Error: transfers.ErrEmptyList}
+		h := NewHandler(&useCase, log)
+		newRequest, _ := http.NewRequest(http.MethodGet, "/transfers", nil)
+		ctx := context.WithValue(newRequest.Context(), server_http.ContextAccountID, "123")
+		newResponse := httptest.NewRecorder()
+		h.ListByID(newResponse, newRequest.WithContext(ctx))
+
+		var response server_http.Error
+		_ = json.Unmarshal(newResponse.Body.Bytes(), &response)
+
+		assert.Equal(t, newResponse.Header().Get("content-type"), server_http.JSONContentType)
+		assert.Equal(t, newResponse.Code, http.StatusNotFound)
+		assert.Equal(t, response.Reason, transfers.ErrTransfersNotFound.Error())
+	})
+
+	t.Run("should return 500 and an error when an unexpected error occourred", func(t *testing.T) {
+		unexpectedError := errors.New("unexpected error")
+		useCase := transfers.UseCaseMock{Error: unexpectedError}
+		h := NewHandler(&useCase, log)
+		newRequest, _ := http.NewRequest(http.MethodGet, "/transfers", nil)
+		ctx := context.WithValue(newRequest.Context(), server_http.ContextAccountID, "123")
+		newResponse := httptest.NewRecorder()
+		h.ListByID(newResponse, newRequest.WithContext(ctx))
+
+		var response server_http.Error
+		_ = json.Unmarshal(newResponse.Body.Bytes(), &response)
+
+		assert.Equal(t, newResponse.Header().Get("content-type"), server_http.JSONContentType)
+		assert.Equal(t, newResponse.Code, http.StatusInternalServerError)
+		assert.Equal(t, unexpectedError.Error(), response.Reason)
+	})
+}
