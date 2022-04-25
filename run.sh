97049f21c65c43d4b60ac31f7ae7821e6ccbd923
Daniel Sender <daniel.saul7@gmail.com>
2022-04-25 13:20:01 -0300

    chore: update environments variables

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #%/bin/bash
 export DB_URL="postgres://postgres:1234@localhost:5432/desafio"
 export API_PORT=":3000"
-export TOKEN_SECRET="bags lime ruts loop dale muse"
+export TOKEN_SECRET="PU3lcBUKmE"
 export EXP_TIME="5m"
 go run main.go
