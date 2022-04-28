1d5b3b46a1da9b79f5ab637e2c45b3fa2e65932d
Daniel Sender <daniel.saul7@gmail.com>
2022-04-28 17:10:36 -0300

    test: remove unnecessary entity creating

--- pkg/domain/accounts/usecases/get_balance_by_id_test.go
+++ pkg/domain/accounts/usecases/get_balance_by_id_test.go
@@ -32,14 +32,11 @@ func TestAccountUseCase_GetBalanceByID(t *testing.T) {
 	})
 
 	t.Run("should return an account balance equal to zero and an error when the id wasn't found", func(t *testing.T) {
-		name := "John Doe"
-		cpf := "11111111030"
-		secret := "123"
-		balance := 10
-		account, _ := entities.NewAccount(name, cpf, secret, balance)
+
+		id := "247eade0-53b2-4ee5-9a3b-0da9afba5700"
 		tests.DeleteAllAccounts(Db)
 
-		getBalance, err := accountUsecase.GetBalanceByID(ctx, account.ID)
+		getBalance, err := accountUsecase.GetBalanceByID(ctx, id)
 
 		assert.NotNil(t, getBalance)
 		assert.Equal(t, err, accounts_usecase.ErrAccountNotFound)
